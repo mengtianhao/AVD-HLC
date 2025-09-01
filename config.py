d7148fa6a9667839056e3cc6baa3b62afe1cac02
mengtianhao <30464726+mengtianhao@users.noreply.github.com>
2025-09-01 18:05:50 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+'''
+@time: 2025/4/16
+
+@ author:
+'''
+
+class Config:
+
+    seed = 10
+
+    # path
+    datafolder = '../data/ptbxl/'
+    # datafolder = '../data/SPH/'
+
+    # exp0, exp1, exp1.1, exp1.1.1, exp2, exp3
+
+    experiment = 'exp0'
+
+    # train model name:  MyView, resnet1d_wang, inceptiontime, fcn_wang, lstm, mobilenetv3_small, ...
+    model_name = 'MyView'
+
+    batch_size = 32
+
+    max_epoch = 100
+
+    lr = 0.001
+
+    device_num = 0
+
+    checkpoints = 'MyView_exp0_checkpoint_best.pth'
+
+config = Config()
